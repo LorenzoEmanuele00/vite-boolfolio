ce202855d10132498a15f48dd69d8bf510bf9ce2
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-02-01 16:41:46 +0100

    add component for card display using props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,25 +1,35 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
-  data() {
-    return {
-        projects: [],
-        baseUrl: 'http://127.0.0.1:8000'
-    }
-  },
-  created() {
-    axios
-    .get(`${this.baseUrl}/api/projects`)
-    .then((resp) => {
-        console.log(resp);
-    })
-  }
+    data() {
+        return {
+            projects: [],
+            baseUrl: 'http://127.0.0.1:8000'
+        };
+    },
+    created() {
+        axios
+            .get(`${this.baseUrl}/api/projects`)
+            .then((resp) => {
+            this.projects = resp.data.result;
+            console.log(this.projects);
+        });
+    },
+    components: { ProjectCard }
 }
 </script>
 
 <template>
-
+  <div class="container">
+    <div class="row row-cols-4 g-3">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project"/>
+      </div>
+    </div>
+  </div>
+  
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios';
+
+export default {
+    props: {
+        project: Object,
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <div class="d-flex align-items-center justify-content-center title_box">
+            <h3 class="p-2 text-center">{{project.title}}</h3>
+        </div>
+        <hr>
+        <p class="p-2">{{project.description}}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .title_box {
+        height: 120px;
+    }
+</style>
