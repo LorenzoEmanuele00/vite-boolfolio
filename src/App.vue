80eabd7e8ca85167ab455f81e94e9bb221650d6b
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-02-01 16:03:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Test</h1>
+</template>
+
+<style lang="scss">
+  @use "./style/general.scss";
+
+</style>
