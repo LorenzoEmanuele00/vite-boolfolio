fd18f0757ade7954a69b984bca28ea592ff9315c
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-02-05 23:01:13 +0100

    add pagination

--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -3,29 +3,53 @@ import axios from 'axios';
 import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
-    data() {
-        return {
-            projects: [],
-            baseUrl: 'http://127.0.0.1:8000'
-        };
-    },
-    created() {
-        axios
-            .get(`${this.baseUrl}/api/projects`)
-            .then((resp) => {
-            this.projects = resp.data.result;
-        });
-    },
-    components: { ProjectCard }
+  components: { ProjectCard },
+  data() {
+      return {
+          projects: [],
+          baseUrl: 'http://127.0.0.1:8000',
+          curPage: 1,
+          lastPage: 1,
+          loading: false,
+      };
+  },
+  created() {
+    this.callPage(1);
+  },
+  methods: {
+    callPage(num) {
+      this.loading = true;
+      axios
+        .get(`${this.baseUrl}/api/projects`, {
+          params: {
+            page: num,
+          }
+        })
+        .then((resp) => {
+        this.projects = resp.data.result.data;
+        this.lastPage = resp.data.result.last_page;
+        console.log(resp)
+        console.log(this.curPage, this.lastPage)
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="container">
+  <div class="container p-3">
     <div class="row row-cols-4 g-3">
       <div class="col" v-for="project in projects" :key="project.id">
         <ProjectCard :project="project"/>
       </div>
+    </div>
+    <div class="text-center">
+      <button class="btn btn-primary me-1" @click="callPage(curPage = curPage - 1)" :disabled="curPage === 1">Prev</button>
+      <button v-for="num in lastPage" class="btn btn-primary me-1" :class="{'btn-success' : num === curPage}" @click="callPage(num)">{{ num }}</button>
+      <button class="btn btn-primary me-1" @click="callPage(curPage = curPage + 1)" :disabled="curPage === lastPage">Next</button>
     </div>
   </div>
 </template>
