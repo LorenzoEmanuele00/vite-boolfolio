640959ab827530da6df3c08c17abf323d9da8359
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-02-05 17:03:45 +0100

    add route for show in router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "./pages/AppHome.vue";
 import AppAbout from "./pages/AppAbout.vue";
 import AppProject from "./pages/AppProject.vue";
+import AppProjectShow from "./pages/AppProjectShow.vue"
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             name: 'Project',
             component: AppProject
         },
+        {
+            path: '/project/:slug',
+            name: 'ProjectShow',
+            component: AppProjectShow
+        }
     ]
 });
 
